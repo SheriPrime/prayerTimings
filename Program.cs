405e64bf82153797dd42a060199b61b0c060eb0f
Muhammad Shaheer <ssheri1000@gmail.com>
2025-09-28 17:42:35 +0500

    Uncommented the lines...

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ class Program
         string geoJson = await client.GetStringAsync(geoUrl);
         var geoDoc = JsonDocument.Parse(geoJson).RootElement;
 
-        Console.WriteLine(geoDoc);
+        // Console.WriteLine(geoDoc);
 
         double lat = geoDoc.GetProperty("lat").GetDouble();
         double lon = geoDoc.GetProperty("lon").GetDouble();
@@ -21,17 +21,17 @@ class Program
         Console.WriteLine($"Detected Location: {geoDoc.GetProperty("city")}, {geoDoc.GetProperty("country")}");
         Console.WriteLine($"Latitude: {lat}, Longitude: {lon}");
 
-        // string prayerUrl = $"https://api.aladhan.com/v1/timings?latitude={lat}&longitude={lon}&method=2";
-        // string prayerJson = await client.GetStringAsync(prayerUrl);
+        string prayerUrl = $"https://api.aladhan.com/v1/timings?latitude={lat}&longitude={lon}&method=2";
+        string prayerJson = await client.GetStringAsync(prayerUrl);
 
-        // var prayerDoc = JsonDocument.Parse(prayerJson).RootElement;
-        // var timings = prayerDoc.GetProperty("data").GetProperty("timings");
+        var prayerDoc = JsonDocument.Parse(prayerJson).RootElement;
+        var timings = prayerDoc.GetProperty("data").GetProperty("timings");
 
-        // Console.WriteLine("\nPrayer Times:");
-        // Console.WriteLine("Fajr: " + timings.GetProperty("Fajr").GetString());
-        // Console.WriteLine("Dhuhr: " + timings.GetProperty("Dhuhr").GetString());
-        // Console.WriteLine("Asr: " + timings.GetProperty("Asr").GetString());
-        // Console.WriteLine("Maghrib: " + timings.GetProperty("Maghrib").GetString());
-        // Console.WriteLine("Isha: " + timings.GetProperty("Isha").GetString());
+        Console.WriteLine("\nPrayer Times:");
+        Console.WriteLine("Fajr: " + timings.GetProperty("Fajr").GetString());
+        Console.WriteLine("Dhuhr: " + timings.GetProperty("Dhuhr").GetString());
+        Console.WriteLine("Asr: " + timings.GetProperty("Asr").GetString());
+        Console.WriteLine("Maghrib: " + timings.GetProperty("Maghrib").GetString());
+        Console.WriteLine("Isha: " + timings.GetProperty("Isha").GetString());
     }
 }

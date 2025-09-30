6c64afe1d45bfb613d1957e0a263b2126939d8c2
Muhammad Shaheer <ssheri1000@gmail.com>
2025-09-30 16:53:13 +0500

    Changed API's to more accurate ones. Added options for different sects.

--- Program.cs
+++ Program.cs
@@ -1,37 +1,91 @@
-﻿using System;
-using System.Net.Http;
+﻿using System.Drawing;
 using System.Text.Json;
-using System.Threading.Tasks;
+using IPGeolocation;
+using Pastel;
 
 class Program
 {
+    enum Salah
+    {
+        fajr,
+        dhuhr,
+        asr,
+        maghrib,
+        isha
+    }
+
+    enum Sect
+    {
+        Shafi,
+        Hanafi,
+        Maliki,
+        Hanbali
+    }
+
+    static String selectSect()
+    {
+        Console.WriteLine("Select your Islamic sect:");
+        Console.WriteLine("1. Shafi");
+        Console.WriteLine("2. Hanafi");
+        Console.WriteLine("3. Maliki");
+        Console.WriteLine("4. Hanbali");
+        Console.Write("Enter the number corresponding to your sect (Default is Hanafi): ");
+        string choice = Console.ReadLine() ?? string.Empty;
+        return choice switch
+        {
+            "1" => Sect.Shafi.ToString(),
+            "2" => Sect.Hanafi.ToString(),
+            "3" => Sect.Maliki.ToString(),
+            "4" => Sect.Hanbali.ToString(),
+            _ => Sect.Hanafi.ToString(),
+        };
+    }
+    static string offsetTime(string time, double offset)
+    {
+        TimeSpan ts = TimeSpan.Parse(time);
+        ts = ts.Add(TimeSpan.FromHours(offset));
+        return ts.ToString(@"hh\:mm");
+    }
+
+
+
     static async Task Main(string[] args)
     {
         using HttpClient client = new HttpClient();
 
-        string geoUrl = "http://ip-api.com/json/";
-        string geoJson = await client.GetStringAsync(geoUrl);
-        var geoDoc = JsonDocument.Parse(geoJson).RootElement;
+        string apiKey = "8dd225fd884548768ba7a93419947505";
 
-        // Console.WriteLine(geoDoc);
+        IPGeolocationAPI api = new IPGeolocationAPI(apiKey);
+        GeolocationParams geoParams = new GeolocationParams();
+        Geolocation location = api.GetGeolocation(geoParams);
 
-        double lat = geoDoc.GetProperty("lat").GetDouble();
-        double lon = geoDoc.GetProperty("lon").GetDouble();
+        Timezone tz = api.GetTimezone();
+        double offset = tz.GetTimezoneOffset();
 
-        Console.WriteLine($"Detected Location: {geoDoc.GetProperty("city")}, {geoDoc.GetProperty("country")}");
+        string lat = double.Parse(location.GetLatitude()).ToString("0.0000");
+        string lon = double.Parse(location.GetLongitude()).ToString("0.0000");
+
+        Console.WriteLine($"Detected Location: {location.GetCity()}, {location.GetCountryName()}");
         Console.WriteLine($"Latitude: {lat}, Longitude: {lon}");
 
-        string prayerUrl = $"https://api.aladhan.com/v1/timings?latitude={lat}&longitude={lon}&method=2";
-        string prayerJson = await client.GetStringAsync(prayerUrl);
+        string sect = selectSect();
+        string prayerUrl = $"https://www.ummahapi.com/api/prayer-times?lat={lat}&lng={lon}&madhab={sect}&method=MuslimWorldLeague";
 
-        var prayerDoc = JsonDocument.Parse(prayerJson).RootElement;
-        var timings = prayerDoc.GetProperty("data").GetProperty("timings");
+        string prayerJson = await client.GetStringAsync(prayerUrl);
+        JsonElement prayerDoc = JsonDocument.Parse(prayerJson).RootElement;
+        JsonElement timings = prayerDoc.GetProperty("data").GetProperty("prayer_times");
 
         Console.WriteLine("\nPrayer Times:");
-        Console.WriteLine("Fajr: " + timings.GetProperty("Fajr").GetString());
-        Console.WriteLine("Dhuhr: " + timings.GetProperty("Dhuhr").GetString());
-        Console.WriteLine("Asr: " + timings.GetProperty("Asr").GetString());
-        Console.WriteLine("Maghrib: " + timings.GetProperty("Maghrib").GetString());
-        Console.WriteLine("Isha: " + timings.GetProperty("Isha").GetString());
+
+        foreach (Salah salah in Enum.GetValues<Salah>())
+        {
+            string prayerName = salah.ToString();
+            string adjustedTime = offsetTime(timings.GetProperty(prayerName).ToString(), offset);
+            Console.WriteLine($"{prayerName}: {adjustedTime}");
+        }
+        Console.WriteLine("\nNote: " + prayerDoc.GetProperty("data").GetProperty("islamic_info").GetProperty("note").GetString());
+
+        Console.WriteLine("\nPress any key to exit...".Pastel(Color.Red));
+        Console.ReadKey();
     }
 }
